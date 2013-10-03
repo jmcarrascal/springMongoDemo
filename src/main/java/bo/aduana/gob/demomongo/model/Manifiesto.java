b6b674eac9515400f12a818d0bd4925f58f50061
esalamanca <esalamanca@aduana.gob.bo>
2013-10-03 18:42:49 -0400

    Creacion de carpetas main/java

---
+++ src/main/java/bo/aduana/gob/demomongo/model/Manifiesto.java
@@ -0,0 +1,11 @@
+package bo.aduana.gob.demomongo.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: esalamanca
+ * Date: 03-10-13
+ * Time: 06:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Manifiesto {
+}
---
+++ src/main/java/bo/aduana/gob/demomongo/service/ManifiestoService.java
@@ -0,0 +1,11 @@
+package bo.aduana.gob.demomongo.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: esalamanca
+ * Date: 03-10-13
+ * Time: 06:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ManifiestoService {
+}
